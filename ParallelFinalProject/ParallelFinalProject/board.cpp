8a0ba7d5a90af6a3e3b5abfda123e6e87823a920
mean0705 <48243382+mean0705@users.noreply.github.com>
2021-11-23 17:33:02 +0800

    add search.cpp .h and EvaluateBoard
    EvaluateBoard : score

--- ParallelFinalProject/ParallelFinalProject/board.cpp
+++ ParallelFinalProject/ParallelFinalProject/board.cpp
@@ -15,7 +15,6 @@ int board_init()
 	for (int i = 0; i < 64; ++i) {
 		board[BColor][i] = init_color[i];
 		board[BPiece][i] = init_piece[i];
-
 	}
 	for (int i = 0; i < 2; i++) {
 		for (int j = 0; j < 64; j++) {
