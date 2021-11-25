3d06122f7cf35d7db769806d5bb01455acbbb32e
mean0705 <leoleo752175@gmail.com>
2021-11-25 17:47:49 +0800

    add ReadBook

--- ParallelFinalProject/ParallelFinalProject/search.cpp
+++ ParallelFinalProject/ParallelFinalProject/search.cpp
@@ -1,9 +1,52 @@
 #include <stdio.h>
 #include <string.h>
+#include <fstream>
+#include <sstream>
+#include <vector>
 #include "board.h"
 #include "search.h"
 #include "DATA.h"
+vector<StepSet> oneGame;
+vector<vector<StepSet>> allGame;
 
+void ReadBook() {
+	ifstream tempin;
+	tempin.open("book.txt");
+
+	string read_line;
+	string token;
+
+
+	while (getline(tempin, read_line)) {
+		if (read_line == "") continue;
+
+		vector<string> arr;
+		istringstream delim(read_line);
+		while (getline(delim, token, ' ')) {
+			arr.push_back(token);
+		} // 
+
+		for (int i = 0; i < arr.size(); i++) {
+			StepSet s1;
+			s1.side = i % 2;
+			s1.step_num = i / 2;
+			s1.step = arr[i].c_str();
+			oneGame.push_back(s1);
+		} // for
+
+		allGame.push_back(oneGame);
+	} // while
+
+	for (int i = 0; i < allGame.size(); i++) {
+		for (int j = 0; j < allGame[i].size(); j++) {
+			cout << "side : " << allGame[i][j].side << endl;
+			cout << "step_num : " << allGame[i][j].step_num << endl;
+			cout << "from : " << allGame[i][j].step << endl;
+		} // for
+	} // for
+
+
+} // ReadBook()
 int EvaluateBoard(int board_t[2][64]) {
 	int score_WHITE = 0;
 	int score_BLACK = 0;
--- ParallelFinalProject/ParallelFinalProject/search.h
+++ ParallelFinalProject/ParallelFinalProject/search.h
@@ -2,7 +2,17 @@
 #pragma warning
 #include "Def.h"
 #include <iostream>
+#include <string> 
 
 int EvaluateBoard(int board_[2][64]);
 int before_search();
 int search(int alpha, int beta, int depth);
+
+void ReadBook();
+
+typedef struct StepSet {
+public:
+    int side;
+    int step_num;
+    string step;
+} StepSet; // StepSet
